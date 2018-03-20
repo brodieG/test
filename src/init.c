06731343be2bb873e8cc5265b9135e0bb649bce4
brodieG <brodieG@users.noreply.github.com>
2018-03-19 21:07:51 -0400

    set up for rchk

---
+++ src/init.c
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2018  Brodie Gaslam
+ *
+ * This file is part of "fansi - ANSI Control Sequence Aware String Functions"
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Go to <https://www.r-project.org/Licenses/GPL-2> for a copy of the license.
+ */
+
+#include "test.h"
+#include <R_ext/Rdynload.h>
+
+static const
+R_CallMethodDef callMethods[] = {
+  {"test_fun", (DL_FUNC) &TEST_test_fun, 1},
+  {NULL, NULL, 0}
+};
+
+void R_init_test(DllInfo *info)
+{
+ /* Register the .C and .Call routines.
+    No .Fortran() or .External() routines,
+    so pass those arrays as NULL.
+  */
+  R_registerRoutines(info, NULL, callMethods, NULL, NULL);
+  R_useDynamicSymbols(info, FALSE);
+  R_forceSymbols(info, FALSE);
+}
+
