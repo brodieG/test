86dc0e07fdd2568c4ccec15d105f2a999f74ede7
brodieG <brodieG@users.noreply.github.com>
2019-01-24 08:20:28 -0500

    merge changes

--- R/test.R
+++ R/test.R
@@ -1,6 +1,9 @@
 #' A document
 #'
 #' Blah Blah
+#'
+#' @useDynLib test, .registration=TRUE, .fixes="TEST_"
+#' @export
 
-test_fun <- function(x) filter(x, rep(1, 4) / 4)
+test_fun <- function(x) .Call(TEST_test_fun, x)
 
